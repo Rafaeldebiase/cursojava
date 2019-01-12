4a13ac6b706437a4fa2040c2b52686d28fd99cde
Rafael Biase <rafaelvitaldebiase@gmail.com>
2019-01-12 00:00:09 -0200

    Criação do banco de dados H2 para teste e inclusão das anotações do JPA

--- src/main/java/com/rafaelbiase/cursojava/domain/Categoria.java
+++ src/main/java/com/rafaelbiase/cursojava/domain/Categoria.java
@@ -2,9 +2,20 @@ package com.rafaelbiase.cursojava.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import net.bytebuddy.dynamic.loading.ClassReloadingStrategy.Strategy;
+
+@Entity
 public class Categoria implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
