febd13d038f749950fb6116ecacae3ed71ec6b05
Rafael Biase <rafaelvitaldebiase@gmail.com>
2019-01-12 09:54:07 -0200

    Persistindo dados no banco H2

--- src/main/java/com/rafaelbiase/cursojava/CursoJavaApplication.java
+++ src/main/java/com/rafaelbiase/cursojava/CursoJavaApplication.java
@@ -1,13 +1,31 @@
 package com.rafaelbiase.cursojava;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.rafaelbiase.cursojava.domain.Categoria;
+import com.rafaelbiase.cursojava.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursoJavaApplication {
+public class CursoJavaApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoJavaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
