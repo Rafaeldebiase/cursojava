49875042c7c0035803070bdae49174cfc2139126
Rafael Biase <rafaelvitaldebiase@gmail.com>
2019-01-12 01:03:16 -0200

    Implementa o serviço e o repositório para classe categoria

--- src/main/java/com/rafaelbiase/cursojava/domain/Categoria.java
+++ src/main/java/com/rafaelbiase/cursojava/domain/Categoria.java
@@ -7,8 +7,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import net.bytebuddy.dynamic.loading.ClassReloadingStrategy.Strategy;
-
 @Entity
 public class Categoria implements Serializable {
 	
--- src/main/java/com/rafaelbiase/cursojava/resources/CategoriaResource.java
+++ src/main/java/com/rafaelbiase/cursojava/resources/CategoriaResource.java
@@ -4,29 +4,27 @@ package com.rafaelbiase.cursojava.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rafaelbiase.cursojava.domain.Categoria;
+import com.rafaelbiase.cursojava.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> Listar() {
-		
-		Categoria cat1 = new Categoria(1, "informática");
-		Categoria cat2 = new Categoria(2, "ecritório");
-		
-		List<Categoria> lista = new ArrayList<>();
-		
-		lista.add(cat2);
-		lista.add(cat1);
-		
-		
-		return lista;
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
