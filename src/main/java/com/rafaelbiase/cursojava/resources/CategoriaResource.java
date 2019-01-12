6be2eadb4d3f312ba351850f454941eb21947644
Rafael Biase <rafaelvitaldebiase@gmail.com>
2019-01-11 23:06:26 -0200

    Testando a primeira classe

--- src/main/java/com/rafaelbiase/cursojava/resources/CategoriaResource.java
+++ src/main/java/com/rafaelbiase/cursojava/resources/CategoriaResource.java
@@ -1,16 +1,32 @@
 package com.rafaelbiase.cursojava.resources;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rafaelbiase.cursojava.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String Listar() {
-		return "Rest está funcionando";
+	public List<Categoria> Listar() {
+		
+		Categoria cat1 = new Categoria(1, "informática");
+		Categoria cat2 = new Categoria(2, "ecritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat2);
+		lista.add(cat1);
+		
+		
+		return lista;
 	}
 
 }
