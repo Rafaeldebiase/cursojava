89d1522cf5eab0934abdc7d8d6814ba4b08d489a
Rafael Biase <rafaelvitaldebiase@gmail.com>
2019-01-13 00:31:31 -0200

    Ajustes no endpoint /categorias/{id} e criação do error handler

--- src/main/java/com/rafaelbiase/cursojava/services/CategoriaService.java
+++ src/main/java/com/rafaelbiase/cursojava/services/CategoriaService.java
@@ -1,5 +1,4 @@
 package com.rafaelbiase.cursojava.services;
-
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import com.rafaelbiase.cursojava.domain.Categoria;
 import com.rafaelbiase.cursojava.repositories.CategoriaRepository;
+import com.rafaelbiase.cursojava.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +16,9 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> 
+		new ObjectNotFoundException("Objeto não encontrado id: " + id 
+				+ "Tipo: " + Categoria.class.getName()));
 	}
 	
 	
