d5030fc5331c1a34751bdb6b05cc852b91e5f991
Rafael Biase <rafaelvitaldebiase@gmail.com>
2019-01-13 16:34:33 -0200

    paginação com parametros opcionais

--- src/main/java/com/rafaelbiase/cursojava/services/CategoriaService.java
+++ src/main/java/com/rafaelbiase/cursojava/services/CategoriaService.java
@@ -4,6 +4,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.rafaelbiase.cursojava.domain.Categoria;
@@ -46,6 +49,12 @@ public class CategoriaService {
 
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPege, String oderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPege, Direction.valueOf(direction), oderBy);
+		return repo.findAll(pageRequest);
+			
 	}
 
 	
